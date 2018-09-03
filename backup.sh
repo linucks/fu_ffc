ebe88eb742def2f51dbfa68e6129e182346aba4b
jmht <linucks42@gmail.com>
2018-09-03 17:44:27 +0100

    Copy file to rundir

--- backup.sh
+++ backup.sh
@@ -8,8 +8,11 @@ password='3:&fTH34'
 wordpress_root=`(cd ../../..; pwd)`
 wpcontent=${wordpress_root}/wp-content
 
+theme_dir=`pwd`
 pushd $wordpress_root
 mysqldump -u${user} -p${password} ${database} > ${wpcontent}/${database}_dump.sql
 cp wp-config.php ${wpcontent}
-tar -czf wp-content.${database}.tar.gz wp-content
+tfile=wp-content.${database}.tar.gz
+tar -czf $tfile wp-content
+mv $tfile $theme_dir
 popd
