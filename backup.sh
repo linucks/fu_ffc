b4739cc8b0bb3a26389085ad7039867e1b8d4b57
linucks <linucks42@gmail.com>
2018-11-07 17:06:39 +0000

    Add notes on restoring

--- backup.sh
+++ backup.sh
@@ -16,3 +16,13 @@ tfile=wp-content.${database}.tar.gz
 tar -czf $tfile wp-content
 mv $tfile $theme_dir
 popd
+
+
+# To restore from the backup, use the ffc_dump.sql file in the wp-content directory and the following commands
+#mysql -u root -p
+#DROP database ffc1;
+#CREATE DATABASE ffc1;
+#USE ffc1;
+#GRANT ALL PRIVILEGES ON ffc1.* TO "ffcuser"@"localhost" IDENTIFIED BY "3:&fTH34";
+#FLUSH PRIVILEGES;
+#SOURCE ./ffc1_dump.sql;
